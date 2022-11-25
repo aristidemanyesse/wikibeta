fa1a5dc2f8417114175ce3a3c7c1a1331e7ce2b1
Aristide Manyessé <aristide.manyesse@jool-int.com>
2022-11-25 22:34:52 +0000

    structuration

--- core/management/commands/extract_data.py
+++ core/management/commands/extract_data.py
@@ -8,19 +8,26 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         contries = [x for x in os.listdir("datas/lot1/") if os.path.isdir("datas/lot1/{}".format(x))]
         
-        year = 2013
-        files = [x for x in os.listdir("datas/lot1/germany") if not os.path.isdir("datas/lot1/germany/{}".format(x))]
-        for file in files:
-            nb = re.findall('\d+', file)
-            if nb[0] == "2":
-                if len(nb) == 1:
-                    edition = "{}-{}".format(year, year+1)
-                else:
-                    edition = "{}-{}".format(year-int(nb[-1]), year-int(nb[-1])+1)
-                path = "datas/lot1/germany/{}".format(edition)
-                os.makedirs(path, exist_ok=True)
-                os.rename("datas/lot1/germany/{}".format(file), "datas/lot1/germany/{}/{}".format(edition, "Bundesliga2.csv"))
-                print(edition)
+        # for path, dirs, files in os.walk("datas/lot1/england/"):
+        #     for dir in dirs:
+        #         os.rename("datas/lot1/england/{}/Premier.csv".format(dir), "datas/lot1/england/{}/{}".format(dir, "Premier League.csv"))
+        #         print(dir)
+        #     break
+        
+        
+        # year = 2013
+        # files = [x for x in os.listdir("datas/lot1/england") if not os.path.isdir("datas/lot1/england/{}".format(x))]
+        # for file in files:
+        #     nb = re.findall('\d+', file)
+        #     if nb[0] == "1":
+        #         if len(nb) == 1:
+        #             edition = "{}-{}".format(year, year+1)
+        #         else:
+        #             edition = "{}-{}".format(year-int(nb[-1]), year-int(nb[-1])+1)
+        #         path = "datas/lot1/england/{}".format(edition)
+        #         os.makedirs(path, exist_ok=True)
+        #         os.rename("datas/lot1/england/{}".format(file), "datas/lot1/england/{}/{}".format(edition, "england SuperLigue.csv"))
+        #         print(edition)
 
 
         #     file.n
