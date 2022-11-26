e46ef58e603901c9184fc7578822ec13a00e84ce
Aristide Manyessé <aristide.manyesse@jool-int.com>
2022-11-26 09:41:28 +0000

    migrate success! let's go !

--- settings/settings.py
+++ settings/settings.py
@@ -79,20 +79,20 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     
-    # 'default': {
-    #     'ENGINE'    : 'django.db.backends.mysql',
-    #     'HOST'      : os.getenv("DB_HOST", "0.0.0.0"),
-    #     'PORT'      : os.getenv("DB_PORT", 3306),
-    #     'USER'      : os.getenv("DB_USER", "root"),
-    #     'PASSWORD'  : os.getenv("DB_PASSWORD", "12345678"),
-    #     'NAME'      : os.getenv("DB_NAME", "wikibet"),
-    # },
-    
-    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE'    : 'django.db.backends.mysql',
+        'HOST'      : os.getenv("DB_HOST", "0.0.0.0"),
+        'PORT'      : os.getenv("DB_PORT", 3306),
+        'USER'      : os.getenv("DB_USER", "root"),
+        'PASSWORD'  : os.getenv("DB_PASSWORD", "12345678"),
+        'NAME'      : os.getenv("DB_NAME", "wikibet"),
+    },
+    
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
