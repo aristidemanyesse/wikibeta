6d8b00632acf7e89a3e9f8c16657c31f13ec5ee0
aristide.manyesse@jool-int.com <aristide.manyesse@jool-int.com>
2023-06-18 19:38:44 +0000

    mise ajour et assainize

--- settings/settings.py
+++ settings/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'PORT'      : os.getenv("DB_PORT", 3306),
         'USER'      : os.getenv("DB_USER", "root"),
         'PASSWORD'  : os.getenv("DB_PASSWORD", "12345678"),
-        'NAME'      : os.getenv("DB_NAME", "wikibet"),
+        'NAME'      : os.getenv("DB_NAME", "dev"),
         'POOL_OPTIONS' : {
             'POOL_SIZE': 1000,
             'MAX_OVERFLOW': 100,
